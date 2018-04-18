ee73b60815e1c6365bd8f0c64642568ecc9e4a75
kwonki199 <kwonki199@naver.com>
2018-04-18 15:10:16 +0900

    총민이꺼 뒤로가기

--- app/src/main/java/edu/android/mainmen/Write/WriteReviewActivity.java
+++ app/src/main/java/edu/android/mainmen/Write/WriteReviewActivity.java
@@ -10,8 +10,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -41,6 +43,10 @@ public class WriteReviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_review);
+
+        // 뒤로가기 버튼 이거랑 아래꺼
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         imageView = findViewById(R.id.imageView);
 
@@ -218,6 +224,16 @@ public class WriteReviewActivity extends AppCompatActivity {
         }
     }
 
+    // 뒤로가기 버튼 이거
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id==android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 //    public void findByMyLocation(View view) {
 //
@@ -228,6 +244,7 @@ public class WriteReviewActivity extends AppCompatActivity {
 //
 //
 //    }
+
 }
 
 
