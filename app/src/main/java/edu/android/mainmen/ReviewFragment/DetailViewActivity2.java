e3c55f0e34dbbecf0fe2e5e17103256fbe5764bf
stu <hcm4398@gmail.com>
2018-04-30 14:50:47 +0900

    디테일뷰 구현 (사진은 안넘어옴)

--- app/src/main/java/edu/android/mainmen/ReviewFragment/DetailViewActivity2.java
+++ app/src/main/java/edu/android/mainmen/ReviewFragment/DetailViewActivity2.java
@@ -1,12 +1,13 @@
-package edu.android.mainmen.Adapter;
+package edu.android.mainmen.ReviewFragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -29,12 +30,18 @@ import java.util.List;
 
 import edu.android.mainmen.Controller.AllFoodDTO;
 import edu.android.mainmen.R;
-import edu.android.mainmen.ReviewFragment.DetailViewActivity2;
 
 import static edu.android.mainmen.Upload.FirebaseUploadActivity.FOOD;
 
-public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
-    
+public class DetailViewActivity2 extends AppCompatActivity {
+
+    TextView ID, textView, textView2, detailAdress;
+    ImageView imageView;
+    ImageView deleteButton;
+    ImageView starButton;
+    TextView heartCount;
+    RatingBar rb;
+
 
     private Context context;
     private List<AllFoodDTO> firebaseData;
@@ -44,36 +51,55 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     private List<String> uidLists;
 
 
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view2);
 
-    public MyAdapter(Context context, List<AllFoodDTO> firebaseData, FirebaseAuth auth, FirebaseDatabase database, FirebaseStorage storage, List<String> uidLists) {
-        this.context = context;
-        this.firebaseData = firebaseData;
-        this.auth = auth;
-        this.database = database;
-        this.storage = storage;
-        this.uidLists = uidLists;
+        ID = findViewById(R.id.item_textView_id);
+        detailAdress = findViewById(R.id.detailAdress);
+        imageView = (ImageView) findViewById(R.id.item_imageView);
+        textView = (TextView) findViewById(R.id.item_textView);
+        textView2 = (TextView) findViewById(R.id.item_textView2);
+        starButton = findViewById(R.id.item_heart_image);
+        heartCount = findViewById(R.id.item_heart_count);
+        rb = findViewById(R.id.rb);
+        deleteButton = (ImageView) findViewById(R.id.item_delete_image);
+
+        Intent intent =getIntent();
+
+        String Location = getIntent().getStringExtra("location");
+        String id = getIntent().getStringExtra("id");
+        String title = getIntent().getStringExtra("title");
+        String desc = getIntent().getStringExtra("desc");
+        float rating = getIntent().getFloatExtra("rating", 0);
+
+        String image = getIntent().getStringExtra("images");
+        int Star = getIntent().getIntExtra("heartCount", 0);
+
+        detailAdress.setText(Location);
+        ID.setText(id);
+        textView.setText(title);
+        textView2.setText(desc);
+        rb.setRating(rating);
+       imageView.setImageURI(Uri.parse(image));
+
+        heartCount.setText(Star + "명이 좋아합니다.");
+
+
+
     }
 
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_board, parent, false);
-
-        return new CustomViewHolder(view);
-    }
-
-    @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
-        ((CustomViewHolder)holder).textView.setText(firebaseData.get(position).title);
-        ((CustomViewHolder)holder).textView2.setText(firebaseData.get(position).description);
-        ((CustomViewHolder)holder).rb.setRating(firebaseData.get(position).ratingScore);
-        Glide.with(holder.itemView.getContext()).load(firebaseData.get(position).imageUrl).into(((CustomViewHolder)holder).imageView);
+        textView.setText(firebaseData.get(position).title);
+        textView2.setText(firebaseData.get(position).description);
+        rb.setRating(firebaseData.get(position).ratingScore);
+        Glide.with(holder.itemView.getContext()).load(firebaseData.get(position).imageUrl).into(imageView);
 
-        ((CustomViewHolder)holder).ID.setText(firebaseData.get(position).userId);
-        ((CustomViewHolder)holder).heartCount.setText(firebaseData.get(position).starCount+"명이 좋아합니다.");
+        ID.setText(firebaseData.get(position).userId);
+        heartCount.setText(firebaseData.get(position).starCount + "명이 좋아합니다.");
         //좋아요 버튼
-        ((CustomViewHolder)holder).starButton.setOnClickListener(new View.OnClickListener() {
+        starButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseUser user = auth.getCurrentUser();
@@ -87,53 +113,24 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
 
         FirebaseUser user = auth.getCurrentUser();
-        if(user!=null) {
+        if (user != null) {
             if (firebaseData.get(position).stars.containsKey(auth.getCurrentUser().getUid())) {
-                ((CustomViewHolder) holder).starButton.setImageResource(R.drawable.ic_heart2);
+                starButton.setImageResource(R.drawable.ic_heart2);
 
             } else {
-                ((CustomViewHolder) holder).starButton.setImageResource(R.drawable.ic_heart1);
+                starButton.setImageResource(R.drawable.ic_heart1);
             }
         }
 
-        ((CustomViewHolder)holder).imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                Intent intent = new Intent(context, DetailViewActivity2.class);
-                intent.putExtra("location",firebaseData.get(position).Location);
-                intent.putExtra("id",firebaseData.get(position).userId);
-                intent.putExtra("title",firebaseData.get(position).title);
-                intent.putExtra("desc",firebaseData.get(position).description);
-                intent.putExtra("rating",firebaseData.get(position).ratingScore);
-                intent.putExtra("images",firebaseData.get(position).imageUrl);
-                intent.putExtra("heartCount",firebaseData.get(position).starCount);
-
-                context.startActivity(intent);
-
-
-            }
-        });
-
-
-
-        ((CustomViewHolder)holder).deleteButton.setOnClickListener(new View.OnClickListener() {
+        deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 delete_content(position);
             }
         });
 
-//        ((CustomViewHolder)holder).imageView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent =new Intent(context, SearchActivity.class);
-//                context.startActivity(intent);
-//            }
-//        });
-  }
-
-
+    }
 
     //글 삭제
     private void delete_content(final int position) {
@@ -200,13 +197,12 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     }
 
 
-    @Override
     public int getItemCount() {
         return firebaseData.size();
     }
 
     private class CustomViewHolder extends RecyclerView.ViewHolder {
-        TextView ID,textView,textView2;
+        TextView ID, textView, textView2;
         ImageView imageView;
         ImageView deleteButton;
         ImageView starButton;
@@ -219,7 +215,7 @@ public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
             imageView = (ImageView) itemView.findViewById(R.id.item_imageView);
             textView = (TextView) itemView.findViewById(R.id.item_textView);
             textView2 = (TextView) itemView.findViewById(R.id.item_textView2);
-            deleteButton = (ImageView)itemView.findViewById(R.id.item_delete_image);
+            deleteButton = (ImageView) itemView.findViewById(R.id.item_delete_image);
             starButton = itemView.findViewById(R.id.item_heart_image);
             heartCount = itemView.findViewById(R.id.item_heart_count);
             rb = itemView.findViewById(R.id.rb);
