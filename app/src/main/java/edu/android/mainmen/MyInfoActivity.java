bfe536f893293785bdfc83f7d23155228f3e15b0
JHBAE93 <bjh7388@naver.com>
2018-05-03 16:38:32 +0900

    작업현황 기능 추가

--- app/src/main/java/edu/android/mainmen/MyInfoActivity.java
+++ app/src/main/java/edu/android/mainmen/MyInfoActivity.java
@@ -124,6 +124,14 @@ public class MyInfoActivity extends AppCompatActivity {
                 }
             });
 
+            holder.workBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MyInfoActivity.this, WorkLogActivity.class);
+                    startActivity(intent);
+                }
+            });
+
         }
 
         @Override
@@ -138,10 +146,9 @@ public class MyInfoActivity extends AppCompatActivity {
             TextView textUserAge;
             ImageView couponBtn;
             ImageView managementReviewBtn;
-            ImageView likeBtn;
             Button noticeBtn;
-            Button nameChangeBtn;
             Button cancelBtn;
+            Button workBtn;
 
 
             public ViewHolder(View itemView) {
@@ -154,6 +161,7 @@ public class MyInfoActivity extends AppCompatActivity {
                 managementReviewBtn = itemView.findViewById(R.id.managementReviewBtn);
                 noticeBtn = itemView.findViewById(R.id.noticeBtn);
                 cancelBtn = itemView.findViewById(R.id.myInfo_cancelBtn);
+                workBtn = itemView.findViewById(R.id.workBtn);
             }
         }
     }
