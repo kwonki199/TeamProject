789936841a708993bc98e37be0743b95aa85cd43
JHBAE93 <bjh7388@naver.com>
2018-05-04 11:28:12 +0900

    내가 작성한글 x 버튼 정상 기능

--- app/src/main/java/edu/android/mainmen/DrawerMenu/MyWritingActivity.java
+++ app/src/main/java/edu/android/mainmen/DrawerMenu/MyWritingActivity.java
@@ -3,12 +3,14 @@ package edu.android.mainmen.DrawerMenu;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import edu.android.mainmen.R;
 
 public class MyWritingActivity extends AppCompatActivity {
 
-
+    Button cancelBtn;
 
     //TODO:  fragment 끼워서 내가 작성한글 보기 구현 완료.!
 
@@ -23,5 +25,14 @@ public class MyWritingActivity extends AppCompatActivity {
             actionBar.hide();
 
         }
+
+        cancelBtn = findViewById(R.id.xbtn_cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
